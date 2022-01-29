3ea53922d5fc965109e10f407fedb4ebeef3a4c9
pro100 <frostymag177@gmail.com>
2022-01-29 12:23:52 +0300

    init

---
+++ src/main/java/sky/pro/skyexceptions23/SkyExceptions23Application.java
@@ -0,0 +1,13 @@
+package sky.pro.skyexceptions23;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyExceptions23Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyExceptions23Application.class, args);
+	}
+
+}
