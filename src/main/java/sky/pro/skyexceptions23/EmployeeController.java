5c53a3785bfcc6b74ae4b54774774c12ab1f69ea
pro100 <frostymag177@gmail.com>
2022-01-29 19:49:24 +0300

    add fix, remove not

--- src/main/java/sky/pro/skyexceptions23/EmployeeController.java
+++ src/main/java/sky/pro/skyexceptions23/EmployeeController.java
@@ -3,6 +3,7 @@ package sky.pro.skyexceptions23;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,9 +15,17 @@ public class EmployeeController {
         this.employeeService = employeeService;
     }
 
-    @GetMapping(path = "/employee/{id}")
+@GetMapping(path = "/employee/{id}")
     public String getFullNameEmployee(@PathVariable("id") Integer id) {
         return employeeService.getFullNameEmployee(id);
+    }
+@GetMapping(path = "/addEmployee")
+    public String addEmployee(@RequestParam("fn") String firstname, @RequestParam("ln") String lastname) {
+        return employeeService.addEmployee(firstname, lastname);
+    }
+@GetMapping(path = "/RMEmployee")
+    public String removeEmployee(@RequestParam("fn") String firstname, @RequestParam("ln") String lastname) {
+        return employeeService.removeEmployee(firstname, lastname);
     }
 
 }
--- src/main/java/sky/pro/skyexceptions23/EmployeeServiceImpl.java
+++ src/main/java/sky/pro/skyexceptions23/EmployeeServiceImpl.java
@@ -7,16 +7,13 @@ import sky.pro.skyexceptions23.classes.Employee;
 import java.util.List;
 
 @Service
-public class EmployeeServiceImpl implements EmployeeService {
+public class EmployeeServiceImpl<employees> implements EmployeeService {
 
     Employee[] employees = {
            new Employee("FirstName1", "LastName1"),
            new Employee("FirstName2", "LastName2"),
            new Employee("FirstName3", "LastName3")
     };
-
-
-
 
     @Override
     public String getEmployee(Integer id) {
@@ -26,22 +23,45 @@ public class EmployeeServiceImpl implements EmployeeService {
     @Override
     public String getFullNameEmployee(Integer id) {
         try {
-           return employees[id].getFirstname() + " " + employees[id].getLastName();
+            return String.join(" ", employees[id].getFirstname(), employees[id].getLastName());
+           //return employees[id].getFirstname() + " " + employees[id].getLastName();
         } catch (ArrayIndexOutOfBoundsException exception) {
             return "badRequest Not Found";
-        } catch (NullPointerException nullPointerException){
-            return "Employee is null";
         }
     }
 
     @Override
     public String addEmployee(String firstName, String lastName) {
-        return null;
+        int arrayLength = employees.length + 1;
+        Employee[] buffer = new Employee[arrayLength];
+        if (firstName != null && lastName != null){
+            for (int i = 0; i < employees.length; i++) {
+
+                    buffer[i] = employees[i];
+
+
+            }
+            buffer[arrayLength - 1] = new Employee(firstName, lastName);
+            employees = buffer;}
+        else {
+            return "data missed, pls fill firstname and lastname";
+        }
+        return firstName + " " + lastName + ": added to rep";
     }
 
     @Override
     public String removeEmployee(String firstName, String lastName) {
-        return null;
+        Employee[] buffer = new Employee[employees.length];
+        if (firstName != null && lastName != null){
+            for (int i = 0; i < employees.length; i++) {
+                    if (!employees[i].equals(firstName) && !employees[i].equals(lastName)){
+                        buffer[i] = employees[i];
+                        employees[i] = buffer [i];
+                    }
+            }
+            employees = buffer;
+        }
+        return "done";
     }
 
     @Override
